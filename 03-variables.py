a8eb5f09ccbee6318e2f3c64e95d967f6cef6a5f
Archit Khanna <architk.222@gmail.com>
2021-10-27 13:44:10 +0530

    Update 03-variables.py

--- 03-variables.py
+++ 03-variables.py
@@ -21,3 +21,31 @@ print(g)
 print(type(a))
 print(type(g))
 print(h)
+
+#Only alpha-numeric and underscore
+#Cannot begin with a number
+#Case sensitive
+#Can begin with only a letter or underscore
+
+i, j, k, = 1, 2, 3 #Multiple assignments
+l, m, n, = 'first', "second",'third'
+o = p = q = 10 #Multiple variables with same value
+
+#global variables are declared outside a function while local are inside.
+#a local variable can be made global by using the keyword global.
+
+def outside():
+    print(a)
+
+def inside():
+    a=2
+    print(a)
+
+def overwrite():
+    global a 
+    a = 22
+
+outside()
+inside()
+overwrite()
+print(a)
