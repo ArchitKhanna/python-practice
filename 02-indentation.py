baf296129659bec0fa8cd21f79436769817fbdf9
Archit Khanna <architk.222@gmail.com>
2021-10-27 13:21:44 +0530

    Variables practice

--- 02-indentation.py
+++ 02-indentation.py
@@ -5,3 +5,4 @@ if 3 > 5:
     print("Three is greater than five!")
 else:
     print("Three is not greater than five!")
+#always maintain the right amount of indentation in a code block
---
+++ 03-variables.py
@@ -0,0 +1,23 @@
+a = 10
+A = 5
+b = 20
+c = -10
+
+print(a)
+print(b)
+print(c)
+print("addition of a and b is: ", a+b)
+print("addition of a and c is: ", a+c)
+print("addition of b and c is: ", b+c)
+print(A+a)
+
+d = str(3)
+e = ' is'
+f = " a number"
+g = d+e+f
+h = float(2)
+
+print(g)
+print(type(a))
+print(type(g))
+print(h)
