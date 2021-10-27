6e9f0403f832daa57f8e3ab7fd615fcb732eb5f2
Archit Khanna <architk.222@gmail.com>
2021-10-27 13:04:49 +0530

    Indentation Practice

---
+++ 01-intro.py
@@ -0,0 +1,2 @@
+print("hello world!")
+print("This is an introduction to python.")
---
+++ 02-indentation.py
@@ -0,0 +1,7 @@
+if 5 > 3: 
+    print("Five is greater than 3!")
+
+if 3 > 5:
+    print("Three is greater than five!")
+else:
+    print("Three is not greater than five!")
